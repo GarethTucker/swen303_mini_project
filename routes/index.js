62047bae9aa7d77064fbc986b69b860c9baad773
Gareth Tucker <Gareth Tucker>
2016-03-17 11:17:14 +1300

    :grin: Basex search is rendering on the page

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,15 @@ client.execute("OPEN Colenso");
 
 /* GET home page. */
 router.get("/",function(req,res){
-client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-" (//name[@type='place'])[1] ",
-function (error, result) {
-if(error){ console.error(error);}
-else {
-res.render('index', { title: 'ECS Video Rental', place: result.result });
-}
-}
-);
+	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+		" (//name[@type='place'])[1] ",
+		function (error, result) {
+			if(error){ console.error(error);}
+			else {
+				res.render('index', { title: 'ECS Video Rental', place: result.result });
+			}
+		}
+	);
 });
 
 
