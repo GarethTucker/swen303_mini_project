588fbc514e0f49cff83af12f6a657f14e43a0998
GarethTucker <garethtucker2012@gmail.com>
2016-03-17 14:15:41 +1300

    Webpage has connections to basex

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
 
 /* GET home page. */
-router.get("/",function(req,res){
+router.post("/",function(req,res){
 	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
 		" (//name[@type='place'])[1] ",
 		function (error, result) {
@@ -15,6 +15,24 @@ router.get("/",function(req,res){
 			}
 		}
 	);
+});
+
+router.get("/search",function(req,res){
+	var q = req.query;
+	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+		" (//name[@type='"+q.query+"'])[1] ",
+		function (error, result) {
+			if(error){ console.error(error);}
+			else {
+				res.render('index', { title: 'ECS Video Rental', place: result.result });
+			}
+		}
+	);
+});
+
+router.post('/', function (req, res) {
+    console.log(req.body.title);
+    res.render('index', {title: 'Colenso', place: req.body.title});
 });
 
 
