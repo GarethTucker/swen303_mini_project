114622b5f2ab9003ab431767d635d94b3195d79e
GarethTucker <garethtucker2012@gmail.com>
2016-03-22 15:40:07 +1300

    Browsing implemented :heartpulse:

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,19 @@ var express = require('express');
 var router = express.Router();
 var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+var prettyData = require('pretty-data').pd;
 client.execute("OPEN Colenso");
 
+var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; "
+
 /* GET home page. */
-router.post("/",function(req,res){
-	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+router.get("/",function(req,res){
+	client.execute(tei +
 		" (//name[@type='place'])[1] ",
 		function (error, result) {
 			if(error){ console.error(error);}
 			else {
-				res.render('index', { title: 'ECS Video Rental', place: result.result });
+				res.render('index', { title: 'ECS Video Rental', place: result.result, database_list: [] });
 			}
 		}
 	);
@@ -19,14 +22,45 @@ router.post("/",function(req,res){
 
 router.get("/search",function(req,res){
 	var q = req.query;
-	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-		" (//name[@type='"+q.query+"'])[1] ",
+	client.execute( tei +
+		" (//name[@type='"+q.bob+"'])[1] ",
 		function (error, result) {
 			if(error){ console.error(error);}
 			else {
-				res.render('index', { title: 'ECS Video Rental', place: result.result });
+				res.render('index', { title: 'ECS Video Rental', database_list: [], place: result.result });
 			}
 		}
+	);
+});
+
+router.get("/browse",function(req,res){
+	client.execute("XQUERY db:list('Colenso')",
+		function (error, result) {
+			if(error){ console.error(error);}
+			else {
+				var list = result.result;
+				var arraylist = list.split("\n");
+				res.render('index', { title: 'Browse', place: 'default', database_list: arraylist });
+			}
+		}
+	);
+});
+
+router.get("/display/*", function(req, res){
+	var url = req.originalUrl;
+	console.log(url);
+	var path = url.replace('/display', 'Colenso');
+	// path = path.shift();
+	console.log('path:'+path);
+	// path = path.join('/');
+	// client.execute(tei + "for $doc in ('Colenso') where matches(document-uri($doc),  '" +path+ "') return $doc",
+		client.execute("XQUERY doc('"+path+"')",
+			function(error, result){
+				if(error){ console.error(error);}
+				else {
+					res.render('index', {title: 'display', database_list: [], xml_doc: result.result});
+				}
+			}
 	);
 });
 
@@ -37,3 +71,5 @@ router.post('/', function (req, res) {
 
 
 module.exports = router;
+
+// XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; for $doc in ('Colenso') where matches(document-uri($doc),  'PrLHadfld-0017.xml') return $doc
