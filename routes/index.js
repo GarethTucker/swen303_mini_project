697649404e5bc1bb246e4bbf08c5ce48ba6f41e0
GarethTucker <garethtucker2012@gmail.com>
2016-03-24 14:38:25 +1300

    :nail_care: Can search by query or text

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 var prettyData = require('pretty-data').pd;
 client.execute("OPEN Colenso");
 
-var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; "
+var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; ";
+
 
 /* GET home page. */
 router.get("/",function(req,res){
@@ -20,27 +21,66 @@ router.get("/",function(req,res){
 	);
 });
 
-router.get("/search",function(req,res){
+router.get("/search/XQUERY",function(req,res){
+	//tei = "XQUERY declare namespace tei = 'http://www.tei-c.org/ns/1.0'; ";
 	var q = req.query;
-	client.execute( tei +
-		" (//name[@type='"+q.bob+"'])[1] ",
+	var input = q.input;
+	console.log("tei+input: "+tei+input);
+	client.execute( (tei+input),
 		function (error, result) {
 			if(error){ console.error(error);}
 			else {
-				res.render('index', { title: 'ECS Video Rental', database_list: [], place: result.result });
+				console.log("result: "+result.result);
+				res.render('index', { title: 'ECS Video Rental', database_list: [], search_result: result.result });
 			}
 		}
 	);
 });
 
-router.get("/browse",function(req,res){
+router.get("/search/Text",function(req,res){
+	//tei = "XQUERY declare namespace tei = 'http://www.tei-c.org/ns/1.0'; ";
+	var q = req.query;
+	var input = q.input;
+	console.log("input: "+input);
+	var full_query = (tei+ "\nfor $t in //p \nwhere matches($t,'"+input+"', 'i') = true() \nreturn db:path($t)")
+	console.log("Full Query: "+full_query)
+	client.execute( (full_query),
+		function (error, result) {
+			if(error){ console.error(error);}
+			else {
+				console.log("result: "+result.result);
+				res.render('index', { title: 'ECS Video Rental', database_list: [], search_result: result.result });
+			}
+		}
+	);
+});
+
+router.get("/browse", function(req,res){
 	client.execute("XQUERY db:list('Colenso')",
 		function (error, result) {
 			if(error){ console.error(error);}
 			else {
-				var list = result.result;
-				var arraylist = list.split("\n");
-				res.render('index', { title: 'Browse', place: 'default', database_list: arraylist });
+				var xml_results = result.result;
+				var xml_lines = xml_results.split("\n");
+				res.render('index', { title: 'Browse', place: 'default', database_list: xml_lines });
+			}
+		}
+	);
+});
+
+router.get("/browse/*", function(req,res){
+	var path = req.originalUrl.replace('/browse', 'Colenso');
+	path = path.substring(0, path.length - 1);
+	console.log("path: "+path);
+	client.execute(tei+"(collection('"+path+"'))",
+		function (error, result) {
+			if(error){ console.error(error);}
+			else {
+				console.log("result.result length: "+result.result.length);
+				var xml_results = result.result;
+				var xml = prettyData.xml(xml_results);
+				// var xml_lines = xml_results.split("\n");
+				res.render('index', { title: 'Browse', place: 'default', database_list: xml_results });
 			}
 		}
 	);
