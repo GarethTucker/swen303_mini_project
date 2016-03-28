c6759fb556fa5823dcfeb3c0026c442566c48a8f
GarethTucker <garethtucker2012@gmail.com>
2016-03-28 20:46:15 +1300

    :earth_africa: Can upload, download and edit

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,23 @@
 var express = require('express');
-var multer = require('multer')
+var multer = require('multer');
 var router = express.Router();
 var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 var prettyData = require('pretty-data').pd;
 var cherrio = require('cheerio');
 client.execute("OPEN Colenso");
-var fs = require('fs')
+var fs = require('fs');
 
 var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; ";
 var storage = multer.memoryStorage();
 var upload = multer({
-	storage: storage
-})
+	storage: storage,
+	dest: './uploads'
+});
 router.use(upload.single('file'));
 
 
-router.get('/submit_page', function (req, res) {
-	res.render('submit_page');
-})
 
-
-
-
-
-/* GET home page. */
 router.get("/",function(req,res){
 	client.execute(tei +
 		" (//name[@type='place'])[1] ",
@@ -37,12 +30,16 @@ router.get("/",function(req,res){
 	);
 });
 
+router.get('/submit_page', function (req, res) {
+	res.render('submit_page');
+});
+
 router.post('/save', function(req, res){
-	console.log("Save")
 	if(req.file){
+		var path = req.file.originalname;
 		var xml = req.file.buffer.toString();
-		console.log('xml: '+xml);
-		client.execute('ADD TO "Colenso" "'+xml+'"', function(error, result){
+		console.log("path:",path)
+		client.execute('ADD TO Colenso/uploads/'+path+' "'+xml+'"', function(error, result){
 			if(error){ console.error(error);}
 			else{ console.log("FILE UPLOADED");}
 		});
@@ -50,97 +47,53 @@ router.post('/save', function(req, res){
 	res.redirect('/')
 });
 
-
-
-
-router.get("/download/id/:docId",function(req,res,next){
-	var docId = req.params.docId;
-	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; (//TEI[@xml:id='" + docId + "'])",
-		function (error, result) {
-			if(error){ console.error(error);}
-			var doc = result.result;
-			console.log("get: ");
-			var filename = 'tei_document.xml';
-			res.writeHead(200, {
-				'Content-Disposition': 'attachment; filename='+filename,
-			});
-			res.write(doc);
-			res.end();
+router.get("/download/*",function(req,res,next){
+	
+	var url = req.originalUrl;
+	var path = url.replace('/download/', '');
+	client.execute("XQUERY doc('"+path+"')",
+		function(error, result) {
+			if (error) {
+				console.error(error);
+			}
+			else {
+				var doc = result.result;
+				var filename = 'tei_document.xml';
+				res.writeHead(200, {
+					'Content-Disposition': 'attachment; filename=' + filename,
+				});
+				res.write(doc);
+				res.end();
+			}
 		}
 	)
 });
 
-router.get("/display/id/:id", function(req, res){
-	var id = req.params.id
-	console.log("id: "+id)
-	var query = tei+"//TEI[@xml:id='"+ id+"']";
-	console.log("query: "+query)
-	client.execute(query, function(error, result){
-		if(error){ console.error(error);}
-		else{
-			console.log("id: "+id)
-			var xml_document = {
-				
-			}
-			res.render('index', {
-				title: 'Display_by_id',
-				xml_doc: {
-					text: result.result,
-					xml_id: id
-				}
-			})
+router.get('/search/', function(req, res){
+	var q = req.query;
+	var raw_input = q.text_search;
+
+	var input = decodeURI(raw_input)
+		.replace(" AND ", '\' ftand \'')
+		.replace(" NOT ", '\' ftnot \'')
+		.replace(" OR ", '\' ftor \'');
+
+
+
+	var full_query = tei +
+	"for $t in *[.//text() contains text '"+input+"' using wildcards]return db:path($t)"
+	client.execute( (full_query), function(error, result) {
+		if (error) {
+			console.error(error);
+		}
+		else {
+			var xml_results = result.result;
+			var xml_lines = xml_results.split('\n');
+			res.render('index', { title: 'Search', database_list: xml_lines, search_result: result.result});
 		}
 	})
-})
 
-router.get("/search/", function(req, res){
-
-	if(req.query.text_search !== ""){
-		var q = req.query;
-		var input = q.text_search;
-		console.log("input.length: "+input.length)
-		console.log("Input: "+input);
-		var full_query = (tei+ "//TEI[. contains text "+input+"]");
-		console.log("Full Query: "+full_query)
-		client.execute( (full_query),
-			function (error, result) {
-				if(error){ console.error(error);}
-				else {
-					console.log("result.result.length: "+result.result.length);
-					var parse = cherrio.load(result.result, {xmlMode: true});
-					var doc_data = parse('TEI');
-					var xmls = [];
-					doc_data.each(function(index, element){
-						element = cherrio(element);
-						xmls.push({
-							title: element.find('title').first().text(),
-							author: element.find('author').first().text(),
-							id: element.attr('xml:id')
-						})
-					})
-					res.render('index', { 
-						title: 'Search Results', 
-						database_list: [], 
-						text_search_result: xmls
-					});
-				}
-			}
-		);
-	}else{
-		var q = req.query;
-		var input = q.query_search;
-		console.log("tei+input: "+tei+input);
-		client.execute( (tei+input),
-			function (error, result) {
-				if(error){ console.error(error);}
-				else {
-					console.log("result: "+result.result);
-					res.render('index', { title: 'ECS Video Rental', database_list: [], query_search_results: result.result });
-				}
-			}
-		);
-	}
-})
+});
 
 router.get("/browse", function(req,res){
 	client.execute("XQUERY db:list('Colenso')",
@@ -158,15 +111,11 @@ router.get("/browse", function(req,res){
 router.get("/browse/*", function(req,res){
 	var path = req.originalUrl.replace('/browse', 'Colenso');
 	path = path.substring(0, path.length - 1);
-	console.log("path: "+path);
 	client.execute(tei+"(collection('"+path+"'))",
 		function (error, result) {
 			if(error){ console.error(error);}
 			else {
-
-				console.log("result.result length: "+result.result.length);
 				var xml_results = result.result;
-				console.log("xml_results; "+xml_results);
 				var xml_lines = xml_results.split("\n");
 				res.render('index', { title: 'Browse', place: 'default', database_list: xml_lines, search_result: result.result});
 			}
@@ -177,7 +126,6 @@ router.get("/browse/*", function(req,res){
 router.get("/display/*", function(req, res){
 	var url = req.originalUrl;
 	var path = url.replace('/display', 'Colenso');
-	console.log('path:'+path);
 		client.execute("XQUERY doc('"+path+"')",
 			function(error, result){
 				if(error){ console.error(error);}
@@ -186,17 +134,37 @@ router.get("/display/*", function(req, res){
 						title: 'display',
 						database_list: [],
 						xml_doc: {
-							text: result.result
-					},
-						search_result: result.result});
+							text: result.result,
+							path: path
+						},
+						search_result: result.result
+					});
 				}
 			}
 	);
 });
 
+router.post("/edit/*", function (req, res) {
+	var url = req.originalUrl;
+	var path = url.replace('/edit/Colenso/', '');
+	var xml = req.body.xml_text;
+	var cherrio_xml = cherrio.load(xml, {xmlMode: true});
+	var xml_id = cherrio_xml('TEI').attr('xml:id');
+	var query = "REPLACE "+path+" "+xml
+	console.log("EDIT: "+query)
+
+	client.execute(query, function(error, result){
+		if(error){ console.error(error);}
+		else{
+			res.redirect('/')
+		}
+
+	});
+});
+
 router.post('/', function (req, res) {
     console.log(req.body.title);
-    res.render('index', {title: 'Colenso', place: req.body.title});
+    res.render('index', {title: 'Colenso'});
 });
 
 
