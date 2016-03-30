70c8a2e3e2f00b1a83a2e327873e6c06213e1564
GarethTucker <garethtucker2012@gmail.com>
2016-03-30 18:31:51 +1300

    delete, refine search and added pictures

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/submit_page', routes);
+app.use('/browse_page', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.get("/",function(req,res){
 		function (error, result) {
 			if(error){ console.error(error);}
 			else {
-				res.render('index', { title: 'ECS Video Rental', place: result.result });
+				res.render('index', { title: 'Colenso', place: result.result });
 			}
 		}
 	);
@@ -51,6 +51,7 @@ router.get("/download/*",function(req,res,next){
 	
 	var url = req.originalUrl;
 	var path = url.replace('/download/', '');
+	console.log("PATH: ", path)
 	client.execute("XQUERY doc('"+path+"')",
 		function(error, result) {
 			if (error) {
@@ -70,56 +71,101 @@ router.get("/download/*",function(req,res,next){
 });
 
 router.get('/search/', function(req, res){
-	var q = req.query;
-	var raw_input = q.text_search;
+	if(req.query.text_search !== ""){
+		var q = req.query;
+		var raw_input = q.text_search;
 
-	var input = decodeURI(raw_input)
-		.replace(" AND ", '\' ftand \'')
-		.replace(" NOT ", '\' ftnot \'')
-		.replace(" OR ", '\' ftor \'');
-
-
-
-	var full_query = tei +
-	"for $t in *[.//text() contains text '"+input+"' using wildcards]return db:path($t)"
-	client.execute( (full_query), function(error, result) {
-		if (error) {
-			console.error(error);
+		var input = decodeURI(raw_input)
+			.replace(" AND ", '\' ftand \'')
+			.replace(" NOT ", '\' ftnot \'')
+			.replace(" OR ", '\' ftor \'');
+		var previous_input = req.query.previous_input;
+		var query;
+		if(previous_input.length > 0) {
+			query = "for $t in *[. contains text '" + input + "' ftand '" + previous_input + "' using wildcards]return db:path($t)";
+		}else{
+			query = "for $t in *[. contains text '" + input + "' using wildcards]return db:path($t)";
 		}
-		else {
-			var xml_results = result.result;
-			var xml_lines = xml_results.split('\n');
-			res.render('index', { title: 'Search', database_list: xml_lines, search_result: result.result});
-		}
-	})
+		console.log("previous_input: ", previous_input )
+		var full_query = tei + query;
+		console.log("full_query: ",full_query);
+		client.execute( (full_query), function(error, result) {
+			if (error) {
+				console.error(error);
+			}
+			else {
+				var xml_results = result.result;
+				var xml_lines = xml_results.split('\n');
+				console.log("QUERY:"+query);
+				res.render('index', {
+					title: 'Search',
+					database_list: xml_lines,
+					search_result: result.result,
+					previous_input: input
+				});
+			}
+		})
+	}else{
+		var q = req.query;
+		var input = q.query_search;
+		console.log("tei+input: "+tei+input);
+		client.execute( (tei+input),
+			function (error, result) {
+				if(error){ console.error(error);}
+				else {
+					console.log("result: "+result.result);
+					res.render('index', {
+						title: 'Colenso',
+						database_list: [],
+						query_search_results: result.result
+					});
+				}
+			}
+		);
+	}
 
 });
 
-router.get("/browse", function(req,res){
+router.get("/browse_page", function(req,res){
 	client.execute("XQUERY db:list('Colenso')",
 		function (error, result) {
 			if(error){ console.error(error);}
 			else {
 				var xml_results = result.result;
 				var xml_lines = xml_results.split("\n");
-				res.render('index', { title: 'Browse', place: 'default', database_list: xml_lines, search_result: result.result});
+				res.render('browse_page', {
+					title: 'Browse',
+					place: 'default',
+					database_list: xml_lines,
+					search_result: result.result,
+					depth: 0,
+					old_folder: 'Colenso/'
+				});
 			}
 		}
 	);
 });
 
-router.get("/browse/*", function(req,res){
-	var path = req.originalUrl.replace('/browse', 'Colenso');
-	path = path.substring(0, path.length - 1);
-	client.execute(tei+"(collection('"+path+"'))",
-		function (error, result) {
-			if(error){ console.error(error);}
-			else {
-				var xml_results = result.result;
-				var xml_lines = xml_results.split("\n");
-				res.render('index', { title: 'Browse', place: 'default', database_list: xml_lines, search_result: result.result});
+router.get("/browse_page/*", function(req,res){
+	var depth = Number(req.query.depth);
+	path = req.query.folder;
+	client.execute('XQUERY let $collection := collection("'+path+'") ' +
+		'for $file in $collection ' +
+		'return db:path($file)',
+			function (error, result) {
+				if(error){ console.error(error);}
+				else {
+					var xml_results = result.result;
+					var xml_lines = xml_results.split("\n");
+					res.render('browse_page', {
+						title: 'Browse', 
+						database_list: xml_lines, 
+						search_result: result.result, 
+						depth: depth+1, 
+						old_folder: req.query.folder
+					});
+				}
 			}
-		}
 	);
 });
 
@@ -148,6 +194,15 @@ router.post("/edit/*", function (req, res) {
 	var url = req.originalUrl;
 	var path = url.replace('/edit/Colenso/', '');
 	var xml = req.body.xml_text;
+	// client.execute("XQUERY " +
+	// 	"try {" +
+	// 	"	let $doc := <invalid/>" +
+	// 		let $schema := '<!ELEMENT root (#PCDATA)>'
+	// 		return validate:dtd($doc, $schema)
+	// 	} catch bxerr:BXVA0001 {
+	// 		'DTD Validation failed.'
+	// 	}
+
 	var cherrio_xml = cherrio.load(xml, {xmlMode: true});
 	var xml_id = cherrio_xml('TEI').attr('xml:id');
 	var query = "REPLACE "+path+" "+xml
@@ -161,6 +216,21 @@ router.post("/edit/*", function (req, res) {
 
 	});
 });
+
+router.get("/delete/*", function (req, res) {
+	console.log("DELETE")
+	var url = req.originalUrl;
+	var path = url.replace('/delete/Colenso/', '');
+	console.log("PATH: ", path)
+	var query = "DELETE "+path
+	client.execute(query, function (error, result) {
+		if(error){ console.error(error);}
+		else{
+			console.log("file was deleted")
+			res.redirect('/')
+		}
+	})
+})
 
 router.post('/', function (req, res) {
     console.log(req.body.title);
